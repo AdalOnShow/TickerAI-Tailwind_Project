bcf593192546a1a51e9a20ffffe0222f1b2d8b7d
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-07-03 21:11:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const darkClass = document.getElementById("darkClass");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  darkClass.classList.toggle("dark");
+});

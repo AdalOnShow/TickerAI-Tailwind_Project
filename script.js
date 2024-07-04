e73a13b865731a9b824619802f94bc6438b26843
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-07-04 08:07:06 +0600

    dark mode problem solved

--- script.js
+++
@@ -1,6 +0,0 @@
-const darkClass = document.getElementById("darkClass");
-const button = document.getElementById("button");
-
-button.addEventListener("click", function () {
-  darkClass.classList.toggle("dark");
-});
